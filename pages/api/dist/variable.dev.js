036e9830564f4a7ba9f6812b1041127ae6b7d36a
jikobouei-capital <adrague.ggoch@gmail.com>
2022-03-01 19:52:26 +0900

    fix components key

--- pages/api/dist/variable.dev.js
+++ pages/api/dist/variable.dev.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.skillCardContents = exports.worksCardContents = exports.hobbysCardContents = exports.url = exports.name = void 0;
+exports.skillCardContents = exports.url = exports.name = void 0;
 var name = "Kyo";
 exports.name = name;
 var url = {
@@ -12,128 +12,6 @@ var url = {
   urlTwitter: "https://twitter.com/Inc_capitalist"
 };
 exports.url = url;
-var hobbysCardContents = [{
-  id: 0,
-  image: require("../../public/travel.png"),
-  title: "Travel　-鉄道旅行-",
-  description: [{
-    id: 0,
-    param: "自分の趣味の中で最も優先度が高く，お金もかかっているものです．鉄道で日本各地に赴いては，自分がその場に生きていたことをカメラで記録しています．"
-  }, {
-    id: 1,
-    param: "今，日本はどんどん鉄路が消えていっている状態です．少しでも，皆様に鉄道に興味を持っていただくための活動もしていきたいと考えております．"
-  }]
-}, {
-  id: 1,
-  image: require("../../public/gadget.png"),
-  title: "Gadget　-PC＆ガジェット-",
-  description: [{
-    id: 0,
-    param: "PCやカメラ，スマートフォンなどのガジェット類がとても好きです．パソコンはPCパーツを一つ一つ選んで組んだり，PC周辺機器でより便利で生産性を上げる品がないか日々探してます．"
-  }, {
-    id: 1,
-    param: "PCパーツも色々調べたり，旅で使うカメラなども選んでいます．"
-  }]
-}, {
-  id: 2,
-  image: require("../../public/anime.png"),
-  title: "Anime&Comic　-アニメ&漫画-",
-  description: [{
-    id: 0,
-    param: "日本の文化，アニメなどもお金が比較的かからない趣味として楽しんでいます．TVを私は捨てたので，VODサービスオンリーで視聴しています．"
-  }, {
-    id: 1,
-    param: "他にも，漫画，ラノベなども嗜んでおります．さらに，二次創作を某小説投稿サイトにて執筆しております．"
-  }]
-}, {
-  id: 3,
-  image: require("../../public/money_kabuken.png"),
-  title: "Assets Management　-資産運用-",
-  description: [{
-    id: 0,
-    param: "今の日本の状況から打開するために，自分の資産の置き方を考えた結果，自身で資産を運用しています．"
-  }, {
-    id: 1,
-    param: "中でも，株式に投資し，少しでも資産を増やす努力をすることを当たり前にし続けた結果趣味となりました．"
-  }]
-}];
-exports.hobbysCardContents = hobbysCardContents;
-var worksCardContents = [{
-  id: 0,
-  image: require("../../public/portrait_e.png"),
-  title: "Portrait Site",
-  description: "紹介サイト作成したいと思ったのでNextの練習も兼ねて作成しました．",
-  useTech: [{
-    id: 0,
-    mark: "devicon-javascript-plain colored"
-  }, {
-    id: 1,
-    mark: "devicon-nextjs-plain-wordmark colored"
-  }, {
-    id: 2,
-    mark: "devicon-materialui-plain colored"
-  }, {
-    id: 3,
-    mark: "devicon-tailwindcss-plain colored"
-  }],
-  repository: "https://github.com/POD-azlamarhyu/portraitSite"
-}, {
-  id: 1,
-  image: require("../../public/toweet_e.png"),
-  title: "Django Tweet App",
-  description: "twitterのクローンをDjangoの練習も兼ねて作成しました．",
-  useTech: [{
-    id: 0,
-    mark: "devicon-python-plain colored"
-  }, {
-    id: 1,
-    mark: "devicon-django-plain-wordmark colored"
-  }, {
-    id: 2,
-    mark: "devicon-javascript-plain colored"
-  }, {
-    id: 3,
-    mark: "devicon-bootstrap-plain colored"
-  }],
-  repository: "https://github.com/POD-azlamarhyu/Twitter_clone_with_Django"
-}, {
-  id: 2,
-  image: '',
-  title: "Stock data scraiping",
-  description: "株のデータを収集するために作成しました．",
-  useTech: [{
-    id: 0,
-    mark: "devicon-python-plain colored"
-  }, {
-    id: 1,
-    mark: "devicon-numpy-original colored"
-  }],
-  repository: "https://github.com/POD-azlamarhyu/getStockdata"
-}, {
-  id: 3,
-  image: require("../../public/watch_e.png"),
-  title: "JS Web Watch",
-  description: "DOMの理解のため，WEB時計を作成しました．",
-  useTech: [{
-    id: 0,
-    mark: "devicon-javascript-plain colored"
-  }],
-  repository: "https://github.com/POD-azlamarhyu/Javascript_light_watch"
-}, {
-  id: 4,
-  image: require("../../public/unity_e.png"),
-  title: "Unity Ball Rolling Game",
-  description: "Unityを使った3Ｄコースゲームです．",
-  useTech: [{
-    id: 0,
-    mark: "devicon-csharp-plain colored"
-  }, {
-    id: 1,
-    mark: "devicon-unity-original colored"
-  }],
-  repository: "https://github.com/POD-azlamarhyu/UnityBallrollgame"
-}];
-exports.worksCardContents = worksCardContents;
 var skillCardContents = [{
   id: 0,
   image: "devicon-python-plain colored",
--- pages/components/organisms/hobbys.js
+++ pages/components/organisms/hobbys.js
@@ -1,8 +1,6 @@
 import React from "react";
 import Header from './header';
 import Footer from './footer';
-import Image from "next/image";
-import { hobbysCardContents } from "../../api/variable";
 
 const HobbysLeftCard = (props) => {
     return (
@@ -51,6 +49,71 @@ const HobbysRightCard = (props) =>{
 };
 
 const Hobbys = () => {
+
+    const hobbysCardContents = [
+        {
+            id: 0,
+            image: require("../../../public/travel.png").default,
+            title: "Travel　-鉄道旅行-",
+            description: [
+                {
+                    id: 0,
+                    param:"自分の趣味の中で最も優先度が高く，お金もかかっているものです．鉄道で日本各地に赴いては，自分がその場に生きていたことをカメラで記録しています．",
+                    
+                },
+                {
+                    id: 1,
+                    param: "今，日本はどんどん鉄路が消えていっている状態です．少しでも，皆様に鉄道に興味を持っていただくための活動もしていきたいと考えております．",
+                }
+            ],
+        },
+        {
+            id: 1,
+            image: require("../../../public/gadget.png").default,
+            title: "Gadget　-PC＆ガジェット-",
+            description: [
+                {
+                    id: 0,
+                    param: "PCやカメラ，スマートフォンなどのガジェット類がとても好きです．パソコンはPCパーツを一つ一つ選んで組んだり，PC周辺機器でより便利で生産性を上げる品がないか日々探してます．",
+                },
+                {
+                    id :1,
+                    param: "PCパーツも色々調べたり，旅で使うカメラなども選んでいます．",
+                }
+            ],
+        },
+        {
+            id: 2,
+            image: require("../../../public/anime.png").default,
+            title: "Anime&Comic　-アニメ&漫画-",
+            description: [
+                {
+                    id: 0,
+                    param: "日本の文化，アニメなどもお金が比較的かからない趣味として楽しんでいます．TVを私は捨てたので，VODサービスオンリーで視聴しています．",
+                },
+                {
+                    id: 1,
+                    param: "他にも，漫画，ラノベなども嗜んでおります．さらに，二次創作を某小説投稿サイトにて執筆しております．",
+                }
+            ],
+        },
+        {
+            id: 3,
+            image: require("../../../public/money_kabuken.png").default,
+            title: "Assets Management　-資産運用-",
+            description: [
+                {
+                    id: 0,
+                    param: "今の日本の状況から打開するために，自分の資産の置き方を考えた結果，自身で資産を運用しています．",
+                },
+                {
+                    id: 1,
+                    param: "中でも，株式に投資し，少しでも資産を増やす努力をすることを当たり前にし続けた結果趣味となりました．"
+                }
+            ]
+        }
+    ];
+
     return (
         <>
             <Header />
