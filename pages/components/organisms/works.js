08df74d9c25732a423baa3248ad75aa7e0c5547b
jikobouei-capital <adrague.ggoch@gmail.com>
2022-04-05 13:32:32 +0900

    fix tecmarks devicon import

--- pages/components/organisms/works.js
+++ pages/components/organisms/works.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./header";
 import Footer from "./footer";
 import GitHub from "@mui/icons-material/GitHub";
+import "devicon";
 
 const worksCardContents = [
     {
